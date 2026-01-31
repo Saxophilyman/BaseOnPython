7b7fd9c1798b8729055bb42ccc2a01143d755683
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2026-01-31 20:45:53 +0400

    Update and rename 001. Annotations_type to 001_Annotations_type.py

---
+++ 001_Annotations_type.py
@@ -0,0 +1,8 @@
+Начну с самого простого и элементарного.
+Аннотация типов стоит уже в следующих простых методах, объявляя типы переменных
+
+def get_len(string: str) -> int:
+  return len(string)
+
+def is_comfort_temperature(temperature: int) -> bool:
+  return temperature > 22
